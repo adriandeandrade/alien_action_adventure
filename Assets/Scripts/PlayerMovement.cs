b792dfb0c14c17e34a373f32f51cc223e8150259
Adrian De Andrade <redfoxhint4@hotmail.com>
2019-02-21 16:03:07 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    Rigidbody rBody;
+
+    Vector3 movement;
+    Vector3 desiredMovement;
+
+    private void Awake()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        float movementMag = Mathf.Clamp(movement.magnitude, 0f, 1f);
+        Vector3 movementNorm = movement.normalized;
+        desiredMovement = movementNorm * movementMag;
+        transform.Translate(desiredMovement * moveSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        //rBody.velocity = (desiredMovement * moveSpeed);
+    }
+}
